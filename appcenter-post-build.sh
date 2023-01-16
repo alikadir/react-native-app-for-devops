ad055dde8773452d74348c35513f36d8821313d5
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2023-01-16 13:55:41 +0300

    Create appcenter-post-build.sh

---
+++ appcenter-post-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "postbuild script"
+echo "DENEME ENVIRONMENT VARIABLE"
+echo $DENEME
+echo "=========== ALL ==="
+echo "ALL ENVIRONMENT VARIABLE"
+printenv
+echo "=========== ALL END ==="
