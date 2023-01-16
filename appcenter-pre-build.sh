17c4d488ae5dddfe2cd1822105efaeab86a63e74
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2023-01-16 13:55:14 +0300

    Create appcenter-pre-build.sh

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "prebuild script"
+echo "DENEME ENVIRONMENT VARIABLE"
+echo $DENEME
+echo "=========== ALL ==="
+echo "ALL ENVIRONMENT VARIABLE"
+printenv
+echo "=========== ALL END ==="
